cd477aa2c6bb58f39d1407c5609112ee017aa1b5
Tim Babaryka <timasky95@gmail.com>
2021-08-23 22:50:36 +0300

    refactor:small changes

--- src/index.js
+++ src/index.js
@@ -37,8 +37,43 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(input) {
+    let result=''
+    let array = input.split('**********')
+    let arrzer =[];
+      for (let i = 0; i < array.length; i++) {
+       let codeletter = array[i].match(/.{10}/g);
+         for( let b = 0; b < codeletter.length; b++) {
+            arrzer.push(parseInt(codeletter[b]).toString()); 
+         }
+        arrzer.push(' ');
+        
+     }
+      let decodedmorseletter = [];
+     for( let i=0; i <arrzer.length; i++){
+        let morseletter = arrzer[i].match(/.{2}/g);
+        
+        if (morseletter === null) {
+        decodedmorseletter.push(' ');
+        result += ' ';
+        continue;
+        };
+        for(let b=0; b < morseletter.length; b++){
+            if ( morseletter[b] === '10') {
+                decodedmorseletter.push('.')
+            } else {
+                decodedmorseletter.push('-')
+            }
+            
+        }
+        let letter =decodedmorseletter.join('').trim(); 
+        decodedmorseletter=[];
+         result += MORSE_TABLE[letter] 
+     }
+  return result.trim();
+     
+
+
 }
 
 module.exports = {
--- src/morsetest.js
+++ src/morsetest.js
@@ -39,6 +39,7 @@ const MORSE_TABLE = {
 
 function decoder(input) {
     input = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"
+    let result=''
     let array = input.split('**********')
     let arrzer =[];
       for (let i = 0; i < array.length; i++) {
@@ -49,33 +50,38 @@ function decoder(input) {
         arrzer.push('');
         
      }
+     
       let decodedmorseletter = [];
      for( let i=0; i <arrzer.length; i++){
         let morseletter = arrzer[i].match(/.{2}/g);
         
         if (morseletter === null) {
-        decodedmorseletter.push(' ');
+        decodedmorseletter.push('');
+        result += ' '
         continue;
         };
+        
         for(let b=0; b < morseletter.length; b++){
-            if ( morseletter[b] === '10') {
-                decodedmorseletter.push('.')
-            } else {
+            if ( morseletter[b] === '11') {
                 decodedmorseletter.push('-')
+                
+            } else {
+                decodedmorseletter.push('.')
             }
-            
         }
+       
         let letter =decodedmorseletter.join('').trim(); 
         decodedmorseletter=[];
-         console.log(MORSE_TABLE[letter]);
-        // console.log(letter);
+        //  console.log(MORSE_TABLE[letter]);
+         result += MORSE_TABLE[letter]
+        
+         
      }
-
-
+  console.log(result)
+  return result;
      
-// console.log(morseletter);
+
 
 };
 
 decoder()
-// 
